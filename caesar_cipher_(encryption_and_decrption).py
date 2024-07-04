92f65b646dfe5fec0094d2f83960db4f7441c174
Nouman Hameed <62693237+nouman6093@users.noreply.github.com>
2024-07-04 18:42:45 +0500

    Create caesar_cipher_(encryption_and_decrption).py

---
+++ caesar_cipher_(encryption_and_decrption).py
@@ -0,0 +1,45 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(direction, text, shift):
+    if direction == "encode":
+        str = ""
+        for i in text:
+            old_index = alphabet.index(i)   #finds index of letter which is about to be replaced
+            new_index = old_index + shift   #finds index of letter after shift
+            new_letter = alphabet[new_index]
+            str += new_letter
+        print(f"The encoded text is: {str}")
+    elif direction == "decode":
+        str = ""
+        for i in text:
+            old_index = alphabet.index(i)   #finds index of letter which is about to be replaced
+            new_index = old_index - shift   #finds index of letter after shift
+            new_letter = alphabet[new_index]
+            str += new_letter
+        print(f"The decoded text is: {str}")
+    else:
+        print("Invalid Input")
+
+print(logo)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
+text = input("Type your message for decryption: ").lower()
+shift = int(input("Type the shift number: "))
+caesar(direction, text, shift)
